a58415dedbc3416a9f99ee70dde63fe0352a90f9
tim.dai@dxcfedora <tim.dai@dxcfedora>
2020-03-17 15:20:48 +1100

    test

--- JS/console_log.js
+++ JS/console_log.js
@@ -1,1 +1,9 @@
-console.log("branch1"); 
+var elements = [
+    'Hydrogen',
+    'Helium',
+    'Lithium',
+    'Beryllium'
+  ];
+
+ var mapLength = elements.map(({ length: lengthFooBArX }) => lengthFooBArX); // [8, 6, 7, 9]
+ console.log(mapLength);
